260195f9a1b5848637e921e98ddd6395a66f4633
Gerry Grewall <gobind99@gmail.com>
2025-04-09 01:28:30 -0400

    Create a report which displays word count and character count
    - loops through each word in the word array setting each to lowercase and then looping through each character to find the number of times a character has been seen to an occurrence count.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_words_array, get_num_character_occurrences
+from stats import get_words_array, get_num_character_occurrences, print_character_count_descending
 
 def get_booktext(filepath):
     with open(filepath, "r") as f:
@@ -6,13 +6,19 @@ def get_booktext(filepath):
         return contents      
    
 def main():
-    contents = get_booktext('books/frankenstein.txt')
+    relative_path = 'books/frankenstein.txt'
+    contents = get_booktext(relative_path)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {relative_path}...")    
 
     word_array = get_words_array(contents)
-    print(len(word_array), "words found in the document")
+    print("----------- Word Count ----------")
+    print("Found", len(word_array), "total words")
 
+    print("--------- Character Count -------")
     num_char_occurrences = get_num_character_occurrences(word_array)
-    print(num_char_occurrences)
+    print_character_count_descending(num_char_occurrences)
 
 if __name__ == '__main__':
     main()
--- stats.py
+++ stats.py
@@ -3,16 +3,36 @@ def get_words_array(contents):
     return word_array
 
 def get_num_character_occurrences(word_array):
-    character_occurrences = {}
-    for word in word_array: 
+    character_occurrences = []
 
+    # Loop through each word
+    for word in word_array: 
         word_lowercase = word.lower()
         split_word = list(word_lowercase)         
 
-        for char in split_word:            
-            if (char in character_occurrences):
-                character_occurrences[char] += 1
-            else: 
-                character_occurrences[char] = 1
-        
+        # Loop through each character
+        for char in split_word:
+            char_found = False
+            
+            if char.isalpha() == False: 
+                continue
+
+            for occurence in character_occurrences:            
+                if occurence["char"] == char:                
+                    occurence["count"] += 1
+                    char_found = True       
+
+            if not char_found:
+                character_occurrences.append({'char': char, 'count': 1})               
+    
     return character_occurrences
+
+def print_character_count_descending(num_char_occurrences):
+    num_char_occurrences.sort(reverse=True, key=sort_on)
+
+    for occurence in num_char_occurrences:
+        print(f"{occurence["char"]}: {occurence["count"]}")    
+    
+def sort_on(dict):
+    return dict["count"]
+
