7746abeae3d59120b94064cde863d5d5ce34452a
Gerry Grewall <gobind99@gmail.com>
2025-04-08 23:49:58 -0400

    Update get_book_text function to read contents and return wordcount to stdout

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def get_booktext(filepath):
     with open(filepath, "r") as f:
         contents = f.read()
-        print(contents)
+        get_word_count(contents)
+
+def get_word_count(contents):
+    word_array = contents.split()
+    print(len(word_array), "words found in the document")
         
 def main():
     get_booktext('books/frankenstein.txt')
